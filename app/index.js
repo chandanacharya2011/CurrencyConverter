c8797911f53d3d44b2fddcc0efc19f9034edefcc
Chandan Acharya <chandan.acharya@alba-golf.com>
2019-06-12 23:04:16 -0500

    Adding Status bar and Logo to Home Screen

--- app/index.js
+++ app/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Currency Converter App from app/index.js</Text>
-    </View>
-  );
-}
+import Home from './screens/Home';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+EStyleSheet.build({
+  $primaryBlue: '#4F6D7A',
+  $white: '#FFFFFF'
 });
+
+export default () => <Home />;
